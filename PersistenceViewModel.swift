4663e8872fc0898717519ad68af48e571c87aaef
nhatcalac <nhatcalac@gmail.com>
2022-07-02 16:11:52 +0700

    // mark

--- PersistenceViewModel.swift
+++ PersistenceViewModel.swift
@@ -44,3 +44,4 @@ class PersistenceViewModel : ObservableObject {
         saveFruit()
     }
 }
+//mark
---
+++ Shared/Component.swift
@@ -0,0 +1,20 @@
+//
+//  Component.swift
+//  With
+//
+//  Created by Nhất Minh on 15/06/2022.
+//
+
+import SwiftUI
+
+struct Component: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Component_Previews: PreviewProvider {
+    static var previews: some View {
+        Component()
+    }
+}
